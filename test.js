e0bc6868b2af44ced0bdf3752d3668499c4f930f
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-08-15 14:25:46 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+//AJAX With JQuery
+                // $.ajax({
+                //     url:"http://localhost:8080/StudentManagement_war_exploded/student",
+                //     type : "POST",
+                //     data:studentJSON,
+                //     headers:{"Content-Type":"application/json"},
+                //     success:(res)=> {
+                //         console.log(JSON.stringify(res));
+                //     },
+                //     error:(res)=>{
+                //         console.error(res);
+                //     }
+                // });
